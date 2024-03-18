b10fb06af8eba678059cc4bf502281abb40fdaed
FreelancerMamun <mamunkhan16345@gmail.com>
2024-03-18 16:13:55 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,162 @@
+(function ($) {
+  'use strict';
+
+  $(window).on('load', function () {
+    // ------- Prealoder ------
+    $('#preloader').delay(300).fadeOut('slow');
+  });
+
+  $(document).ready(function () {
+    // ------------- sticky header --------------
+
+    function sticky_header() {
+      var wind = $(window);
+      var sticky = $('header');
+      wind.on('scroll', function () {
+        var scroll = wind.scrollTop();
+        if (scroll < 100) {
+          sticky.removeClass('sticky');
+        } else {
+          sticky.addClass('sticky');
+        }
+      });
+    }
+
+    // ------------- back to top --------------
+
+    // $(window).on('scroll', function () {
+    //   if ($(this).scrollTop() > 600) {
+    //     $('.back-to-top').fadeIn(200);
+    //   } else {
+    //     $('.back-to-top').fadeOut(200);
+    //   }
+    // });
+
+    // $('.back-to-top').on('click', function (event) {
+    //   event.preventDefault();
+    //   $('html, body').animate(
+    //     {
+    //       scrollTop: 0,
+    //     },
+    //     1500
+    //   );
+    // });
+
+    // ------------- Hamburger menu --------------
+
+    // $('.hamburger-menu').on('click', function () {
+    //   $('.hamburger-menu .line-top, .ofcavas-menu').toggleClass('current');
+    //   $('.hamburger-menu .line-center').toggleClass('current');
+    //   $('.hamburger-menu .line-bottom').toggleClass('current');
+    // });
+
+    // ------------- slider --------------
+
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 0,
+      items: 1,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      nav: true,
+      dots: false,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        390: {
+          items: 1,
+        },
+        575: {
+          items: 1,
+        },
+        768: {
+          items: 1,
+        },
+        992: {
+          items: 1,
+        },
+      },
+      navText: [
+        "<i class='fa-solid fa-chevron-left'></i>",
+        "<i class='fa-solid fa-chevron-right'></i>",
+      ],
+    });
+  });
+})(jQuery);
+
+// ====== Language Dropdowen
+
+// const dropdowenelement = document.querySelector('.drop_element');
+
+// const LanguageBtn = document.querySelector('.dropdown-toggle');
+
+// LanguageBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   if (!dropdowenelement.classList.contains('active-dropdown')) {
+//     dropdowenelement.classList.add('active-dropdown');
+//   } else {
+//     dropdowenelement.classList.remove('active-dropdown');
+//   }
+// });
+
+// =======  Carousel Slider
+
+const select = document.querySelector('.select');
+const options_list = document.querySelector('.options-list');
+const options = document.querySelectorAll('.option');
+
+//show & hide options list
+select.addEventListener('click', () => {
+  options_list.classList.toggle('active');
+});
+
+const element = `<span>
+<img src="img/Ellipse 2.svg" style="width: 25px" alt="">
+EN
+</span>`;
+
+const element2 = `
+<div class="option">
+                      <img src="img/Flag_of_Canada_(leaf).svg.png" style="width: 25px" alt="">
+                      <span>CN</span>
+                    </div>`;
+
+//select option
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    options.forEach((option) => {
+      option.classList.remove('selected');
+    });
+
+    select.querySelector('span').innerHTML = option.innerHTML;
+
+    if (!options_list.classList.contains('active')) {
+      options_list.classList.add('active');
+      option.innerHTML = element2;
+    } else {
+      options_list.classList.remove('active');
+      option.innerHTML = element;
+    }
+
+    option.classList.add('selected');
+  });
+});
+
+// ===== Wishlist Action Toggle
+
+const wishlistButton = document.querySelectorAll('.save-wisthlist');
+
+console.log(wishlistButton);
+
+wishlistButton.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!button.classList.contains('active-button')) {
+      button.classList.add('active-button');
+    } else {
+      button.classList.remove('active-button');
+    }
+  });
+});
